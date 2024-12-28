ca8dfc4424e5eb4082381fa6f473acf1cb330bcd
softwareEngneering <softwarelearning66@gmail.com>
2024-12-27 17:55:31 -0800

    Update app.js

--- app.js
+++ app.js
@@ -44,7 +44,6 @@ client.on('ready', () => {
 
 // إعداد Express
 app.get('/', (req, res) => {
-                        <img src="${qrCodeUrl}" alt="QR Code">
 
     if (qrCodeUrl) {
         res.send(`
