9d80b1ae907cd96a84375d61fac13b1587f8f742
softwareEngneering <softwarelearning66@gmail.com>
2024-12-28 09:12:13 -0800

    Update app.js

--- app.js
+++ app.js
@@ -29,9 +29,10 @@ const client = new Client({
 client.on('qr', (qr) => {
     console.log('✅ QR Code received. Open your browser to scan it.');
     qrcode.toDataURL(qr, (err, url) => {
-
+    qrcode.generate(qr, { small: true });
+    console.log('Scan the QR code to log in.');
         qrCodeUrl = url; // تخزين QR Code كصورة Base64
-                    console.log('"${qrCodeUrl}"🚀 Bot is scneer and connected to WhatsApp!');
+                    // console.log('"${qrCodeUrl}"🚀 Bot is scneer and connected to WhatsApp!');
 
     });
 });
