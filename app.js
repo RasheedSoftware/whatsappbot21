a8471ddb3d052193bb54662fd46f1a63f128c753
softwareEngneering <softwarelearning66@gmail.com>
2025-01-08 14:44:13 -0800

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 
 // استخدام المنفذ من البيئة أو استخدام منفذ افتراضي
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 const {
     addBlockedWord,
